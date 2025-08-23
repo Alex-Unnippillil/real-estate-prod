2cddac7397610d812c0177107555e829084bdd58
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-23 15:02:05 -0700

    Add Redis rate limiter and WAF bot mitigation config

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import { authMiddleware } from "./middleware/authMiddleware";
+import { rateLimiterMiddleware } from "./middleware/rateLimiter";
 /* ROUTE IMPORT */
 import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
@@ -28,9 +29,9 @@ app.get("/", (req, res) => {
   res.send("This is home route");
 });
 
-app.use("/applications", applicationRoutes);
-app.use("/properties", propertyRoutes);
-app.use("/leases", leaseRoutes);
+app.use("/applications", rateLimiterMiddleware, applicationRoutes);
+app.use("/properties", rateLimiterMiddleware, propertyRoutes);
+app.use("/leases", rateLimiterMiddleware, leaseRoutes);
 app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
